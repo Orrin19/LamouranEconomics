80fbc4f1052c23de8369d76a9480089aa7c34860
Yershat <mr.ershat@gmail.com>
2022-08-11 21:44:22 +0600

    alpha test v.1

---
+++ countries/countries.js
@@ -0,0 +1,133 @@
+const Lodogia = {
+    Rural: 5805708,
+    Urban: 732721
+}
+const Shushia = {
+    Rural: 5891928,
+    Urban: 889712
+}
+
+const Eshar = {
+    Rural: 6097671,
+    Urban: 857494
+}
+
+const Celtania = {
+    Rural: 138322,
+    Urban: 612718
+}
+const Tsunzhou = {
+    Rural: 4057502,
+    Urban: 711859
+}
+const Suidal = {
+    Rural: 3805078,
+    Urban: 546118
+}
+const DuliGou = {
+    Rural: 1711489,
+    Urban: 361809
+}
+const IgtanGou = {
+    Rural: 3657571,
+    Urban: 552242
+}
+const Festehus = {
+    Rural: 1013743,
+    Urban: 80224
+}
+const Barvaria = {
+    Rural: 2025180,
+    Urban: 544227
+}
+const Edildor = {
+    Rural: 1276773,
+    Urban: 267224
+}
+const Kehia = {
+    Rural: 1957604,
+    Urban: 370042
+}
+const Brabermain = {
+    Rural: 1614084,
+    Urban: 530551
+}
+const Velmisia = {
+    Rural: 2524374,
+    Urban: 423806
+}
+const Belorabia = {
+    Rural: 875127,
+    Urban: 79376
+}
+const Yamoto = {
+    Rural: 1251822, 
+    Urban: 212044
+}
+const Faen = {
+    Rural: 1289734,
+    Urban: 221837
+}
+//-
+const Gobania = {
+    Rural: 1343368,
+    Urban: 139503
+}
+const Scorpio = {
+    Rural: 939358,
+    Urban: 186095
+}
+const Kanakia = {
+    Rural: 1190352,
+    Urban: 303003
+}
+const Syrantur = {
+    Rural: 299362,
+    Urban: 39202
+}
+const Sardary = {
+    Rural: 732337,
+    Urban: 137213
+}
+//-
+const Nurkenia = {
+    Rural: 119684,
+    Urban: 60018
+}
+const Turchiny = {
+    Rural: 663766,
+    Urban: 200569,
+}
+const Scorobey = {
+    Rural: 177419,
+    Urban: 40967,
+}
+const Arraney = {
+    Rural: 343148,
+    Urban: 48783,
+}
+const ThreeCityUnion = {
+    Rural: 287123,
+    Urban: 177659,
+}
+const Doszhan = {
+    Rural: 130980,
+    Urban: 40780,
+}
+const Kude = {
+    Rural: 12202,
+    Urban: 13581,
+}
+const Samat = {
+    Rural: 91252,
+    Urban: 28046,
+}
+const Parahi = {
+    Rural: 77608,
+    Urban: 20064,
+}
+
+module.exports = {Lodogia, Shushia, Eshar, Celtania, Tsunzhou, Suidal, DuliGou, IgtanGou, Barvaria, Edildor, Kehia, 
+    Brabermain, Velmisia, Belorabia, Yamoto, Faen, Gobania, Scorpio, Kanakia, Syrantur, Sardary, Nurkenia, Turchiny,
+    Scorobey, Arraney, ThreeCityUnion, Doszhan, Kude, Samat, Parahi,Festehus
+};
