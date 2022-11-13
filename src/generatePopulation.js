d67eff03279fe36c0d4cb8ff5193210d03f3fb2a
Orrin19 <daniil.orrin@yandex.ru>
2022-11-13 11:58:07 +0700

    taxes in table

--- src/generatePopulation.js
+++ src/generatePopulation.js
@@ -1,16 +1,16 @@
-var rural = Countries[country].rural;
-var urban = Countries[country].urban;
-var all = rural + urban;
-var noble = all * 0.051;
-var priest = all * 0.011;
-var merchant = rural * 0.01 + urban * 0.15;
-var peasant = rural * 0.84;
-var worker = urban * 0.71 + rural * 0.1;
-var urb = Math.floor((urban / all) * 10000) / 100;
-var gos = Math.floor(((noble + merchant) / 3 / all) * urb * 10000) / 100;
-var poorTax = Math.floor(peasant / 100) / 100;
-var middleTax = Math.floor((worker + merchant) / 100) / 100;
-var richTax = Math.floor((priest + noble) / 100) / 100;
+const rural = Countries[country].rural;
+const urban = Countries[country].urban;
+const all = rural + urban;
+const noble = all * 0.051;
+const priest = all * 0.011;
+const merchant = rural * 0.01 + urban * 0.15;
+const peasant = rural * 0.84;
+const worker = urban * 0.71 + rural * 0.1;
+const urb = Math.floor((urban / all) * 10000) / 100;
+const gos = Math.floor(((noble + merchant) / 3 / all) * urb * 10000) / 100;
+const poorTax = Math.floor(peasant / 100) / 100;
+const middleTax = Math.floor((worker + merchant) / 100) / 100;
+const richTax = Math.floor((priest + noble) / 100) / 100;
 document.getElementById('pep').innerHTML = Math.floor(all / 1000);
 document.getElementById('gos').innerHTML = gos;
 document.getElementById('urb').innerHTML = urb;
@@ -19,34 +19,34 @@ document.getElementById('rurpep').innerHTML = Math.floor(rural / 10) / 100;
 document.getElementById('male').innerHTML = Math.floor((all * 0.52) / 10) / 100;
 document.getElementById('female').innerHTML =
   Math.floor((all * 0.48) / 10) / 100;
-var p = document.getElementById('poor');
-var pt = document.getElementById('poorTaxPercent');
-var m = document.getElementById('middle');
-var mt = document.getElementById('middleTaxPercent');
-var r = document.getElementById('rich');
-var rt = document.getElementById('richTaxPercent');
-p.innerHTML = poorTax * 1.1;
-m.innerHTML = middleTax * 1.1;
-r.innerHTML = richTax * 1.1;
+const p = document.getElementById('poor');
+const pt = document.getElementById('poorTaxPercent');
+const m = document.getElementById('middle');
+const mt = document.getElementById('middleTaxPercent');
+const r = document.getElementById('rich');
+const rt = document.getElementById('richTaxPercent');
+p.innerHTML = (poorTax * 1.1).toFixed(2);
+m.innerHTML = (middleTax * 1.1).toFixed(2);
+r.innerHTML = (richTax * 1.1).toFixed(2);
 pt.innerHTML = 10;
 mt.innerHTML = 10;
 rt.innerHTML = 10;
-var ptf = document.getElementById('ptf');
+const ptf = document.getElementById('ptf');
 function poorTaxFix() {
-  var val = ptf.value;
+  const val = ptf.value;
   pt.innerHTML = val;
-  var end = poorTax + (poorTax * val) / 100;
-  p.innerHTML = Math.floor(end * 100) / 100;
+  const end = poorTax + (poorTax * val) / 100;
+  p.innerHTML = end.toFixed(2);
 }
 function middleTaxFix() {
-  var val = mtf.value;
+  const val = mtf.value;
   mt.innerHTML = val;
-  var end = middleTax + (middleTax * val) / 100;
-  m.innerHTML = Math.floor(end * 100) / 100;
+  const end = middleTax + (middleTax * val) / 100;
+  m.innerHTML = end.toFixed(2);
 }
 function richTaxFix() {
-  var val = rtf.value;
+  const val = rtf.value;
   rt.innerHTML = val;
-  var end = richTax + (richTax * val) / 100;
-  r.innerHTML = Math.floor(end * 100) / 100;
+  const end = richTax + (richTax * val) / 100;
+  r.innerHTML = end.toFixed(2);
 }
