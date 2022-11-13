7004664c18a759ef54e9383933d0f8c7847aac70
Orrin19 <daniil.orrin@yandex.ru>
2022-11-13 14:28:34 +0700

    new map structure

---
+++ src/map.js
@@ -0,0 +1,45 @@
+const regions = new Map()
+  .set('nz', 'Нижние Земли')
+  .set('sn', 'Снофельт')
+  .set('vz', 'Верхние Земли')
+  .set('nb', 'Нижний Бамбуг')
+  .set('sr', 'Сырантур')
+  .set('vb', 'Верхний Бамбуг')
+  .set('vd', 'Вадэрд')
+  .set('lz', 'Лодогийский залив')
+  .set('vm', 'Вельмиссия')
+  .set('bm', 'Буйное море')
+  .set('rp', 'Рабийские пески')
+  .set('ss', 'Старые степи')
+  .set('nm', 'Новое море')
+  .set('kn', 'Княжества')
+  .set('tr', 'Тарк')
+  .set('dc', 'Дочехия')
+  .set('bv', 'Бават')
+  .set('ab', 'Аберката')
+  .set('gs', 'Гошар')
+  .set('bs', 'Бушар')
+  .set('ym', 'Ямотания')
+  .set('an', 'Антаковия')
+  .set('cz', 'Чжун-Ту')
+  .set('kr', 'Корская равнина')
+  .set('mp', 'Море призраков')
+  .set('nl', 'Низовья Луриниса')
+  .set('kl', 'Клиттам')
+  .set('dz', 'Джуджух');
+
+document.body.style.zoom = 0.3;
+//document.body.style.overflow = 'hidden';
+const regionName = document.querySelector('.map-container__region-name');
+regions.forEach((name, id) => {
+  const region = document.getElementById(id);
+
+  region.addEventListener('mouseenter', function () {
+    regionName.style.display = 'block';
+    regionName.innerHTML = name;
+  });
+
+  region.addEventListener('mouseleave', function () {
+    regionName.style.display = 'none';
+  });
+});
